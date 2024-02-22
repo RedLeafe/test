a4bc6aa677327ab413267be5d4e6e50625bba900
FrostyX5 <brandonhoang7541@gmail.com>
2024-02-21 19:52:15 -0800

    WIP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+</script>
+
+<image>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+	/>
+</image>
+
+<h1 class="main_header" style="text-align:center">Welcome to Rate My Openings!</h1>
+
+<h2>come rate your favorites</h2>
+
+<style>
+	:global(body) {
+		background: black;
+	}
+	.main_header {
+		font-size: 50px;
+		font-family: 'Courier New', Courier, monospace;
+		padding: 20px 10px;
+		color: white;
+		text-shadow: 0 0 50px #eb21b6;
+	}
+</style>
