d8164dc012abdc808295315e4aabc10652be4fb4
FrostyX5 <brandonhoang7541@gmail.com>
2024-02-21 19:56:01 -0800

    Fixed issue with github added non merged files

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,11 @@
+
+
+<script>
+    import { authStore } from '../../lib/stores/authStore.js';
+    authStore.subscribe(curr => {
+        console.log(authStore.current);
+    });
+</script>
+
+<h1>Profile</h1>    
+<p>Welcome </p> 
